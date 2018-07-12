94e0723d4eb2cbb0fd26cfd7ea0bcd44e5716f37
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-12 22:09:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,8 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   configure do
-  enable :sessions
-  set :session_secret, "secret"
-end
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
   
+
 end
